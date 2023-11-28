8612dd43cef1580466cecdfabc5d41ef43f22064
yaayimded <143564303+yaayimded@users.noreply.github.com>
2023-11-28 21:54:42 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,9 @@
+birth_year = input('Enter your birth year: ')
+# int(birth_year) function to convert a string into an integer
+age = 2023 - int(birth_year)
+print(age)
+
+int()
+float()
+bool()
+str()
