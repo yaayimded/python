9b4d2c1df488178f40f88ab8fb472e4260912406
yaayimded <143564303+yaayimded@users.noreply.github.com>
2023-11-28 23:55:37 +0100

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,6 @@
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('y')) #returns the index of the character or sequence of characters given
+print(course.replace('for', '4'))
+print('Python' in course) #sees if the sequence or character is in the string
